6e09edb227e9f027649f8039a038a83d2b6776c1
erfanamirij5-oss <erfanamirij5@gmail.com>
2026-09-05 10:23:53 +0330

    Load export center desktop styling

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import './font.css';
 import './standards.css';
 import './heatmap.css';
 import './ui-fixes.css';
+import './export-center.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
