594bb7d97668a1aea3679bcd8943786992ca36ff
erfanamirij5-oss <erfanamirij5@gmail.com>
2026-09-05 03:40:55 +0330

    Apply bundled Vazirmatn after Persian styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import '@fontsource-variable/vazirmatn/wght.css';
 import { App } from './App';
 import './styles.css';
 import './persian.css';
+import './font.css';
 import './heatmap.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
