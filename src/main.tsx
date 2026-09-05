d83d3f006d07938e861b692045026a38db23747d
erfanamiriyazd <erfanamirij5@gmail.com>
2026-09-06 00:31:15 +0330

    Load RC readability fixes after UI styles

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import './standards.css';
 import './heatmap.css';
 import './ui-fixes.css';
 import './export-center.css';
+import './rc-readability.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
